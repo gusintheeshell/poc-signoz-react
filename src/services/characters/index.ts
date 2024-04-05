3f90af72af175b4fc4db5f187ec271d9989698cc
Gustavo Barbosa <gusbdev@gmail.com>
2024-04-05 18:50:37 -0300

    feat: add fetchCharactersByPage function to characters service

--- src/services/characters/index.ts
+++ src/services/characters/index.ts
@@ -6,3 +6,10 @@ export const fetchCharacters = async (): Promise<CharacterResponse> => {
   const { data } = await api.get("/character");
   return data;
 };
+
+export const fetchCharactersByPage = async (
+  page: number
+): Promise<CharacterResponse> => {
+  const { data } = await api.get(`/character/?page=${page}`);
+  return data;
+};
