53f96effd2715d000924d7cd4123bdd8c2663921
Gustavo Barbosa <gusbdev@gmail.com>
2024-04-04 23:27:45 -0300

    feat: add QueryClientProvider wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App.tsx";
 import { rickAndMorty } from "@/themes";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider theme={rickAndMorty}>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={rickAndMorty}>
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
