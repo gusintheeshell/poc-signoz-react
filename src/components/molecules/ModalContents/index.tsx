0130ac076fa7a0726af19b700b890c598379b2eb
Gustavo Barbosa <gusbdev@gmail.com>
2024-04-05 18:04:35 -0300

    feat: add ModalContents component to dynamically render content based on cardType

---
+++ src/components/molecules/ModalContents/index.tsx
@@ -0,0 +1,44 @@
+import { FC, useCallback } from "react";
+
+import { useApp } from "@/context";
+import CustomModal from "@/components/atoms/CustomModal";
+
+import CharacterCards from "../CharacterCard";
+import EpisodeAccordion from "../EpisodeAccordion";
+import LocationAccordion from "../LocationAccordion";
+
+interface ModalContentsProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onOpen: () => void;
+}
+
+const ModalContents: FC<ModalContentsProps> = ({ isOpen, onClose }) => {
+  const {
+    state: { cardType },
+  } = useApp();
+
+  const renderContents = useCallback(() => {
+    switch (cardType) {
+      case "Personagens":
+        return <CharacterCards />;
+      case "Episódios":
+        return <EpisodeAccordion />;
+      case "Locais":
+        return <LocationAccordion />;
+    }
+  }, [cardType]);
+
+  return (
+    <CustomModal
+      title={cardType}
+      isOpen={isOpen}
+      onClose={onClose}
+      onLoadMore={() => {}}
+      children={<>{renderContents()}</>}
+      size="xxl"
+    />
+  );
+};
+
+export default ModalContents;
