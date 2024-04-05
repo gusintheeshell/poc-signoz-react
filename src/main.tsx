a4fb64f157a88f4fe3722ac1b69db7a365554173
Gustavo Barbosa <gusbdev@gmail.com>
2024-04-04 23:25:04 -0300

    feat: add ChakraProvider wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx";
+import { rickAndMorty } from "@/themes";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={rickAndMorty}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
