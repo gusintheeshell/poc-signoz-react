e55558dff17716d0c0c2709a7bb2e8a729a8422d
Gustavo Barbosa <gusbdev@gmail.com>
2024-04-05 14:30:42 -0300

    feat: add AppContext with provider and custom hook for state management

---
+++ src/context/index.tsx
@@ -0,0 +1,34 @@
+import { createContext, Dispatch, FC, useContext, useReducer } from "react";
+import { AppAction, AppState } from "./action";
+import { appReducer } from "./reducer";
+
+interface AppProviderProps {
+  children: JSX.Element;
+}
+
+const AppContext = createContext<
+  { state: AppState; dispatch: Dispatch<AppAction> } | undefined
+>(undefined);
+
+const AppProvider: FC<AppProviderProps> = ({ children }) => {
+  const [state, dispatch] = useReducer(appReducer, {
+    cardType: "Personagens",
+  });
+
+  return (
+    <AppContext.Provider value={{ state, dispatch }}>
+      <>{children}</>
+    </AppContext.Provider>
+  );
+};
+
+const useApp = () => {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error("Context not defined");
+  }
+
+  return context;
+};
+
+export { AppProvider, useApp };
