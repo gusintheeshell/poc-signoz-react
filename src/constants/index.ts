f2076297ebe85774dd92eb96b7665299a9c3d1ef
Gustavo Barbosa <gusbdev@gmail.com>
2024-04-05 00:30:22 -0300

    feat: Add CARDS_CONTENT constant for homepage card details

---
+++ src/constants/index.ts
@@ -0,0 +1,14 @@
+export const CARDS_CONTENT = [
+  {
+    image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+    title: "Personagens",
+  },
+  {
+    image: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+    title: "Episódios",
+  },
+  {
+    image: "https://rickandmortyapi.com/api/character/avatar/6.jpeg",
+    title: "Locais",
+  },
+];
