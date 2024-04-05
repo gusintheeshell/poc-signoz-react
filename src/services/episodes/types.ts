c802879f17fe37d637238d92eb9c5368908b53e1
Gustavo Barbosa <gusbdev@gmail.com>
2024-04-05 09:49:43 -0300

    feat: add Episode and EpisodesResponse interfaces for episode service in typescript

---
+++ src/services/episodes/types.ts
@@ -0,0 +1,31 @@
+export interface Episode {
+  id: number;
+  name: string;
+  air_date: string;
+  episode: string;
+  characters: string[];
+  url: string;
+  created: string;
+}
+
+export interface EpisodesResponse {
+  info: Info;
+  results: Result[];
+}
+
+export interface Info {
+  count: number;
+  pages: number;
+  next: string;
+  prev: null;
+}
+
+export interface Result {
+  id: number;
+  name: string;
+  air_date: string;
+  episode: string;
+  characters: string[];
+  url: string;
+  created: string;
+}
