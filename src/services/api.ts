9548cbd3b0abe5070b7d9818dff64b127a283b95
Gustavo Barbosa <gusbdev@gmail.com>
2024-04-06 10:26:03 -0300

    feat: enhance axios imports and define response types in api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,14 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
 
 const baseURL = "https://rickandmortyapi.com/api";
 
 const api: AxiosInstance = axios.create({
   baseURL,
 });
+
+export type Options = AxiosRequestConfig;
+export type Response<T> = AxiosResponse & {
+  data: T;
+};
 
 export default api;
