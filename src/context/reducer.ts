15df663050c85e09f19382b91c45e1e68fdb2fa7
Gustavo Barbosa <gusbdev@gmail.com>
2024-04-07 02:49:55 -0300

    feat: add reset page number action to context reducer

--- src/context/reducer.ts
+++ src/context/reducer.ts
@@ -6,6 +6,8 @@ export const appReducer = (state: AppState, action: AppAction): AppState => {
       return { ...state, cardType: action.payload };
     case "SET_PAGE_NUMBER":
       return { ...state, pageNumber: action.payload };
+    case "SET_PAGE_RESET":
+      return { ...state, pageNumber: 1 };
     default:
       return state;
   }
