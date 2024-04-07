f1c5510189879a3019160b8c851f9fb56406a71c
Gustavo Barbosa <gusbdev@gmail.com>
2024-04-07 15:53:33 -0300

    refactor: location service functions to directly return data from API response

--- src/services/locations/index.ts
+++ src/services/locations/index.ts
@@ -2,13 +2,13 @@ import { apiFetch } from "@/services/apiFetch";
 import { LocationsResponse } from "@/services/locations/types";
 
 export const fetchLocations = async (): Promise<LocationsResponse> => {
-  const response = await apiFetch<LocationsResponse>("/location");
-  return response;
+  const { data } = await apiFetch<LocationsResponse>("/location");
+  return data;
 };
 
 export const fetchLocationsByPage = async (
   page: number
 ): Promise<LocationsResponse> => {
-  const response = await apiFetch<LocationsResponse>(`/location/?page=${page}`);
-  return response;
+  const { data } = await apiFetch<LocationsResponse>(`/location/?page=${page}`);
+  return data;
 };
