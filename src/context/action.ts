e6ed7a37eef8193131ed9a68ea2a530c41e5e44d
Gustavo Barbosa <gusbdev@gmail.com>
2024-04-07 02:49:27 -0300

    feat: add SET_PAGE_RESET action and update AppAction type in action.ts

--- src/context/action.ts
+++ src/context/action.ts
@@ -7,10 +7,12 @@ export type AppState = {
 
 export const SET_CARD_TYPE = "SET_CARD_TYPE";
 export const SET_PAGE_NUMBER = "SET_PAGE_NUMBER";
+export const SET_PAGE_RESET = "SET_PAGE_RESET";
 
 export type AppAction =
   | {
       type: typeof SET_CARD_TYPE;
       payload: AppState["cardType"];
     }
-  | { type: typeof SET_PAGE_NUMBER; payload: AppState["pageNumber"] };
+  | { type: typeof SET_PAGE_NUMBER; payload: AppState["pageNumber"] }
+  | { type: typeof SET_PAGE_RESET; payload?: AppState["pageNumber"] };
