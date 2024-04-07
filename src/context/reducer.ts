4696326f6b6e060f00c6b36781c0bf3ab3127ef5
Gustavo Barbosa <gusbdev@gmail.com>
2024-04-07 01:25:42 -0300

    feat: add SET_PAGE_NUMBER case to reducer for managing page number state

--- src/context/reducer.ts
+++ src/context/reducer.ts
@@ -4,6 +4,8 @@ export const appReducer = (state: AppState, action: AppAction): AppState => {
   switch (action.type) {
     case "SET_CARD_TYPE":
       return { ...state, cardType: action.payload };
+    case "SET_PAGE_NUMBER":
+      return { ...state, pageNumber: action.payload };
     default:
       return state;
   }
