5873d6bccae8aa4d35213568edbd784892c3b993
Gustavo Barbosa <gusbdev@gmail.com>
2024-04-07 03:07:00 -0300

    feat: add fetchEpisodesByPage function and import apiFetch in episodes service

--- src/services/episodes/index.ts
+++ src/services/episodes/index.ts
@@ -1,7 +1,15 @@
 import api from "@/services/api";
+import { apiFetch } from "@/services/apiFetch";
 import { EpisodesResponse } from "./types";
 
 export const fetchEpisodes = async (): Promise<EpisodesResponse> => {
   const { data } = await api.get("/episode");
   return data;
+};
+
+export const fetchEpisodesByPage = async (
+  page: number
+): Promise<EpisodesResponse> => {
+  const response = await apiFetch<EpisodesResponse>(`/location/?page=${page}`);
+  return response;
 };
