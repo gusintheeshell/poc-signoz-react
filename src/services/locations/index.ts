8037d559b678067d61dbabbc25bd6bafbadfebe0
Gustavo Barbosa <gusbdev@gmail.com>
2024-04-07 03:04:12 -0300

    refactor: fetchLocations and add fetchLocationsByPage function in locations service

--- src/services/locations/index.ts
+++ src/services/locations/index.ts
@@ -2,6 +2,13 @@ import { apiFetch } from "@/services/apiFetch";
 import { LocationsResponse } from "@/services/locations/types";
 
 export const fetchLocations = async (): Promise<LocationsResponse> => {
-  const { data } = await apiFetch<LocationsResponse>("/location");
-  return data;
+  const response = await apiFetch<LocationsResponse>("/location");
+  return response;
+};
+
+export const fetchLocationsByPage = async (
+  page: number
+): Promise<LocationsResponse> => {
+  const response = await apiFetch<LocationsResponse>(`/location/?page=${page}`);
+  return response;
 };
