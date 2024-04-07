131a6d49151c914aca2f47ad09b01769c0f853e0
Gustavo Barbosa <gusbdev@gmail.com>
2024-04-07 01:25:26 -0300

    feat: add pageNumber to AppState and introduce SET_PAGE_NUMBER action type

--- src/context/action.ts
+++ src/context/action.ts
@@ -2,11 +2,15 @@ import { CardTypes } from "@/types";
 
 export type AppState = {
   cardType: CardTypes | null;
+  pageNumber: number;
 };
 
 export const SET_CARD_TYPE = "SET_CARD_TYPE";
+export const SET_PAGE_NUMBER = "SET_PAGE_NUMBER";
 
-export type AppAction = {
-  type: typeof SET_CARD_TYPE;
-  payload: AppState["cardType"];
-};
+export type AppAction =
+  | {
+      type: typeof SET_CARD_TYPE;
+      payload: AppState["cardType"];
+    }
+  | { type: typeof SET_PAGE_NUMBER; payload: AppState["pageNumber"] };
