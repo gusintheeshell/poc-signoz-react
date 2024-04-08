37a46029290035d873501c5bb3a65328018e8532
Gustavo Barbosa <gusbdev@gmail.com>
2024-04-08 08:52:20 -0300

    refactor: apiFetch in apiFetch.ts to improve readability and error handling

--- src/services/apiFetch.ts
+++ src/services/apiFetch.ts
@@ -1,14 +1,23 @@
 import { traceSpan } from "@/helpers/tracing";
 import api, { Options, Response } from "@/services/api";
 
+/**
+ * Makes a fetch request to the API.
+ *
+ * @param {string} url - The URL to fetch.
+ * @param {Options} [options] - The options for the fetch request.
+ * @returns {Promise<Response<T>>} - A promise that resolves to the API response.
+ * @throws {Error} - If an error occurs during the fetch request.
+ */
 export const apiFetch = async <T extends {}>(
   url: string,
   options?: Options
 ): Promise<Response<T>> => {
-  if (options && options.method && options.method !== "GET") {
-    options.headers = Object.assign(options.headers || {}, {
+  if (options?.method !== "GET") {
+    options!.headers = {
+      ...options?.headers,
       "Content-Type": "application/json",
-    });
+    };
   }
 
   return traceSpan(url, async () => {
