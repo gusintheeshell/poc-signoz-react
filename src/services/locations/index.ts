e7ea546c84e311344e4934fc4b62fba73192bad5
Gustavo Barbosa <gusbdev@gmail.com>
2024-04-08 09:47:23 -0300

    doc: add documentation and implement fetchLocationsByPage function in locations service

--- src/services/locations/index.ts
+++ src/services/locations/index.ts
@@ -1,11 +1,22 @@
 import { apiFetch } from "@/services/apiFetch";
 import { LocationsResponse } from "@/services/locations/types";
 
+/**
+ * Fetches locations from the API.
+ *
+ * @returns {Promise<LocationsResponse>} A promise that resolves to the locations response.
+ */
 export const fetchLocations = async (): Promise<LocationsResponse> => {
   const { data } = await apiFetch<LocationsResponse>("/location");
   return data;
 };
 
+/**
+ * Fetches locations by page number.
+ *
+ * @param page - The page number to fetch locations from.
+ * @returns A promise that resolves to a LocationsResponse object.
+ */
 export const fetchLocationsByPage = async (
   page: number
 ): Promise<LocationsResponse> => {
